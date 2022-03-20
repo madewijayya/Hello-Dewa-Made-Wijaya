bc87186f0269a133dd775c4b698039751dc51470
madewijayya <wijayajaya2002@gmail.com>
2022-03-20 10:09:46 +0800

    first commit

---
+++ kamis lat 1.py
@@ -0,0 +1,6 @@
+hari= input("masukan hari:")
+if hari=="Senin" or hari=="selasa" or hari=="Rabu"or hari=="Kamis" or hari=="Jum'at" or hari== "sabtu" or hari=="minggu":
+    print("valid")
+else :
+    print("tidak valid")
+    
